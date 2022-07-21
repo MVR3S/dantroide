112c7dac7c529726fb73893353c3fe1a98b0b87a
MVR3S <niccolo.dallolio18@iti-marconi.edu.it>
2022-07-21 11:10:41 +0200

    Update Socket.js

--- src/app/js/Socket.js
+++ src/app/js/Socket.js
@@ -1,16 +1,12 @@
 export class WebSocketClient {
 
     constructor() {
-        this.url = 'wss://192.168.188.140:8484';
+        this.url = 'ws://192.168.188.140:8484';
         this.websocket;
     }
 
     SocketConnection() {
-        this.websocket = new WebSocket(this.url, {
-            protocolVersion: 8,
-            origin: 'https://192.168.188.140:8484',
-            rejectUnauthorized: false
-            });
+        this.websocket = new WebSocket(this.url)
             
         console.log("Connesso");
     }
