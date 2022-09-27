a044713785e15efea5e227b2fdd2d6fdc7be0922
MVR3S <niccolo.dallolio18@iti-marconi.edu.it>
2022-09-27 09:11:12 +0200

    Update

--- src/app/js/Socket.js
+++ src/app/js/Socket.js
@@ -1,18 +1,26 @@
 export class WebSocketClient {
 
     constructor() {
-        this.url = 'ws://192.168.205.148:8484';
+        this.url = 'ws://192.168.1.2:8484';
         this.websocket;
     }
 
     SocketConnection() {
         this.websocket = new WebSocket(this.url)
-            
-        console.log("Connesso");
     }
     
     SocketMessage(i) {
-        this.websocket.send(i);
-        console.log("Messaggio inviato: ", i);
+        if (!this.websocket || this.websocket.readyState != this.websocket.OPEN) {
+            this.SocketConnection();
+            setTimeout(() => {
+                    console.log("Delayed for 1 second.");
+                    SocketMessage(i)
+            }, "1000");
+        } else {
+            console.log("Connnected");
+            this.websocket.send(i);
+            console.log("Messaggio inviato: ", i);
+        }
+
     }
 }
