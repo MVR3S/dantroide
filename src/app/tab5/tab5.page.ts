323b45fb78bc064dcc37953a1c606a01d66cd992
mure <niccolo.dallolio18@iti-marconi.edu.it>
2022-10-06 22:50:13 +0200

    Update

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -17,12 +17,12 @@ import {WebSocketClient} from '../js/Socket';
 import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: 'app-tab5',
+  templateUrl: 'tab5.page.html',
+  styleUrls: ['tab5.page.scss']
 })
 
-export class Tab2Page {
+export class Tab5Page {
 
   player: Howl = null;
   isplaying = false;
